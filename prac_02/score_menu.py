78118522a440754ca90f407eba3fc3db01721a9a
lachlancrabbJCU <140355342+lachlancrabbJCU@users.noreply.github.com>
2024-05-31 12:37:50 +1000

    Add functions

--- prac_02/score_menu.py
+++ prac_02/score_menu.py
@@ -1,5 +1,8 @@
 """mod doc"""
-MENU = """menu:"""
+MENU = """(G)et score
+(P)rint result
+(S)how stars
+(Q)uit"""
 
 
 def main():
@@ -7,16 +10,43 @@ def main():
     choice = input(">>> ").upper()
     while choice != "Q":
         if choice == "G":
-            pass
+            score = get_valid_score()
         elif choice == "P":
-            pass
+            category = determine_category(score)
+            print(f"Score of {score} is {category}")
         elif choice == "S":
-            pass
+            print_stars(score)  # Print score number of stars
         else:
             print("Invalid Choice.")
         print(MENU)
         choice = input(">>> ").upper()
     print("Thank you for using the score menu\nGood Bye")
+
+
+def get_valid_score() -> float:
+    """Get a valid score between low and high"""
+    score = float(input("Score: "))
+    while score < 0 or score > 100:
+        print(f"invalid score, must be between 0 and 100 inclusive")
+        score = float(input("Score: "))
+    return score
+
+
+def determine_category(score: float) -> str:
+    """Determine score category based on score."""
+    if score < 0 or score > 100:
+        return "Invalid score"
+    elif score < 50:
+        return "Bad"
+    elif score < 90:
+        return "Passable"
+    else:
+        return "Excellent"
+
+
+def print_stars(length):
+    """Print number of stars equal to length"""
+    print('*' * int(length))
 
 
 main()
