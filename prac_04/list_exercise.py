2b609fd33d9f74a26981222b59365781cb3cebfc
lachlancrabbJCU <140355342+lachlancrabbJCU@users.noreply.github.com>
2024-06-16 13:34:55 +1000

    Add username checker code

--- prac_04/list_exercise.py
+++ prac_04/list_exercise.py
@@ -6,6 +6,15 @@ def main():
     numbers = get_numbers()
     print_report(numbers)
 
+    usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface',
+                 'BaseStdIn', 'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer',
+                 'bob']
+    input_username = input("username: ")
+    if input_username in usernames:
+        print("Access Granted")
+    else:
+        print("Access Denied")
+
 
 def print_report(numbers):
     """Print information about numbers."""
