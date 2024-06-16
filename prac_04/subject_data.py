d7d3d70aabc9209e8ee54a101aa6d4cae5f8678c
lachlancrabbJCU <140355342+lachlancrabbJCU@users.noreply.github.com>
2024-06-16 12:50:13 +1000

    Add print subject details function

--- prac_04/subject_data.py
+++ prac_04/subject_data.py
@@ -8,11 +8,12 @@ FILENAME = "subject_data.txt"
 
 def main():
     data = load_data()
-    print(data)
+    print_subject_details(data)
 
 
 def load_data():
     """Read data from file formatted like: subject,lecturer,number of students."""
+    data = []
     input_file = open(FILENAME)
     for line in input_file:
         print(line)  # See what a line looks like
@@ -21,9 +22,18 @@ def load_data():
         parts = line.split(',')  # Separate the data into its parts
         print(parts)  # See what the parts look like (notice the integer is a string)
         parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
+        data.append(parts)
         print(parts)  # See if that worked
         print("----------")
     input_file.close()
+    return data
+
+
+def print_subject_details(data):
+    name_max_length = max((len(parts[1]) for parts in data))  # find max width of name
+    students_max_length = max((len(str(parts[2])) for parts in data))  # find max width of number of students
+    for parts in data:
+        print(f"{parts[0]} is taught by {parts[1]:{name_max_length}} and has {parts[2]:{students_max_length}} students")
 
 
 main()
