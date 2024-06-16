eba22b32eb46a442afa12e804b7e6dedba002a2c
lachlancrabbJCU <140355342+lachlancrabbJCU@users.noreply.github.com>
2024-06-16 14:20:42 +1000

    Add duplicate handling and list sorting

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -3,13 +3,21 @@ import random
 
 MIN_VALUE = 1
 MAX_VALUE = 45
+NUMBER_RANDOM_NUMBERS = 6
 
 
 def main():
     number_of_quick_picks = int(input("How many \"quick picks\" do you wish to generate? "))
     for i in range(number_of_quick_picks):
-        for j in range(6):
-            print(random.randint(MIN_VALUE, MAX_VALUE), end=" ")
+        quick_pick = []
+        for j in range(NUMBER_RANDOM_NUMBERS):
+            random_int = random.randint(MIN_VALUE, MAX_VALUE)
+            while random_int in quick_pick:
+                random_int = random.randint(MIN_VALUE, MAX_VALUE)
+            quick_pick.append(random_int)
+        sorted_quick_pick = sorted(quick_pick)
+        for number in sorted_quick_pick:
+            print(f"{number:2} ", end="")
         print()
 
 
