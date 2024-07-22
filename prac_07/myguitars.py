0951bd3eb22a35dfcb28dddcbe966b494dbfeebb
lachlancrabbJCU <140355342+lachlancrabbJCU@users.noreply.github.com>
2024-07-22 20:22:52 +1000

    Complete More Guitars exercise

--- prac_07/myguitars.py
+++ prac_07/myguitars.py
@@ -2,7 +2,7 @@
 CP1404 Week 07 Practical
 My Guitars
 Start Time: 19:40
-End Time: 20:
+End Time: 20:20
 """
 from guitar import Guitar
 import csv
@@ -14,11 +14,14 @@ def main():
     print("My guitars!")
     guitars = []
     load_guitars(guitars)
-    display_guitars(guitars.sort())
+    get_guitars(guitars)
+    guitars.sort()
+    print_guitars(guitars)
+    save_guitars(guitars)
 
 
 def load_guitars(guitars):
-    """Guitar file reader version using the csv module."""
+    """Guitar file reader using the csv module."""
     in_file = open(FILENAME, 'r', newline='')
     reader = csv.reader(in_file)  # use default dialect, Excel
     for row in reader:
@@ -26,7 +29,19 @@ def load_guitars(guitars):
     in_file.close()
 
 
-def display_guitars(guitars):
+def get_guitars(guitars):
+    """Get guitar information from user until a blank name is entered."""
+    name = input("Name: ")
+    while name != "":
+        year = int(input("Year: "))
+        cost = float(input("Cost: "))
+        print(f"{name} ({year}) : ${cost:,.2f} added.")
+        guitars.append(Guitar(name, year, cost))
+        print()
+        name = input("Name: ")
+
+
+def print_guitars(guitars):
     """Print list of guitars with proper formatting."""
     print("\nThese are my guitars:")
     max_name_width = max(len(guitar.name) for guitar in guitars)
@@ -35,6 +50,15 @@ def display_guitars(guitars):
         vintage_string = " (vintage)" if guitar.is_vintage() else ""
         print(f"Guitar {i}: {guitar.name:>{max_name_width}} ({guitar.year}), worth ${guitar.cost:{max_cost_width},.2f}"
               f"{vintage_string}")
+
+
+def save_guitars(guitars):
+    """Guitar file writer using the csv module."""
+    out_file = open(FILENAME, 'w', newline="")
+    writer = csv.writer(out_file)
+    for guitar in guitars:
+        writer.writerow([guitar.name, guitar.year, guitar.cost])
+    out_file.close()
 
 
 main()
