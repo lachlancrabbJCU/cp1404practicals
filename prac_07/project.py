464d22c1a313176fe1c91dd5e420f00bf54b1c93
lachlancrabbJCU <140355342+lachlancrabbJCU@users.noreply.github.com>
2024-07-22 21:55:09 +1000

    Add __repr__ method to Project class

--- prac_07/project.py
+++ prac_07/project.py
@@ -14,3 +14,7 @@ class Project:
         self.priority = priority
         self.cost_estimate = cost_estimate
         self.completion_percentage = completion_percentage
+
+    def __repr__(self):
+        return (f"{self.name}, start: {self.start_date}, priority {self.priority}, estimate: ${self.cost_estimate:.2f},"
+                f" completion: {self.completion_percentage}%")
