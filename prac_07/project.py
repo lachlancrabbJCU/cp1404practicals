02f82e3af7d66cfaa31370d97dec76b603646241
lachlancrabbJCU <140355342+lachlancrabbJCU@users.noreply.github.com>
2024-07-22 20:33:28 +1000

    Begin Project Management exercise

---
+++ prac_07/project.py
@@ -0,0 +1,16 @@
+"""
+CP1404 Week 07 Practical
+Project
+"""
+
+
+class Project:
+    """Represent a Project object."""
+
+    def __init__(self, name, start_date, priority, cost_estimate, completion_percentage):
+        """Initialise a Project instance."""
+        self.name = name
+        self.start_date = start_date
+        self.priority = priority
+        self.cost_estimate = cost_estimate
+        self.completion_percentage = completion_percentage
---
+++ prac_07/project_management.py
@@ -0,0 +1,13 @@
+"""
+CP1404 Week 07 Practical
+Project Management
+Start Time: 20.30
+End Time:
+"""
+from project import Project
+
+def main():
+    pass
+
+
+main()
