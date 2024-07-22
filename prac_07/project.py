91da412e3bbc34362a5af7c1f16bb0e5164e871a
lachlancrabbJCU <140355342+lachlancrabbJCU@users.noreply.github.com>
2024-07-22 23:47:49 +1000

    Completed project management exercise

--- prac_07/project.py
+++ prac_07/project.py
@@ -2,6 +2,7 @@
 CP1404 Week 07 Practical
 Project
 Estimate Time: 3 Hours
+Actual Time: 4 hours 15 mins
 """
 import datetime
 
@@ -18,12 +19,15 @@ class Project:
         self.completion_percentage = completion_percentage
 
     def __repr__(self):
+        """String representation of Project object."""
         return (
             f"{self.name}, start: {self.start_date.strftime('%d/%m/%y')}, priority {self.priority}, estimate: ${self.cost_estimate:.2f},"
             f" completion: {self.completion_percentage}%")
 
     def __lt__(self, other):
+        """Return date compare for less than sign operator."""
         return self.start_date < other.start_date
 
     def is_complete(self):
+        """Return true if project in 100% complete."""
         return self.completion_percentage == 100
--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -2,7 +2,7 @@
 CP1404 Week 07 Practical
 Project Management
 Start Time: 20.30
-End Time:
+End Time: 23.45
 """
 from project import Project
 import datetime
