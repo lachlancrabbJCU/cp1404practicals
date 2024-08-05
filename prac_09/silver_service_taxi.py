ae757f18976277e7a9ad9f802bd656774599bf6f
lachlancrabbJCU <140355342+lachlancrabbJCU@users.noreply.github.com>
2024-08-05 20:13:15 +1000

    Complete silver service taxi exercise

---
+++ prac_09/silver_service_taxi.py
@@ -0,0 +1,21 @@
+"""CP1404 Prac 09 Silver Service Taxi"""
+
+from taxi import Taxi
+
+
+class SilverServiceTaxi(Taxi):
+    """Represent a SilverServiceTaxi object."""
+    flagfall = 4.50
+
+    def __init__(self, name: str, fuel: int, fanciness: float):
+        """Initialise a SilverServiceTaxi instance."""
+        super().__init__(name, fuel)
+        self.price_per_km = fanciness * Taxi.price_per_km
+
+    def __str__(self):
+        """Return a string like a Taxi but with an added flagfall."""
+        return f"{super().__str__()} plus flagfall of ${self.flagfall:.2f}"
+
+    def get_fare(self):
+        """Return the price for the taxi trip."""
+        return self.price_per_km * self.current_fare_distance + self.flagfall
