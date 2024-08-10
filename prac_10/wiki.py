c189927367f8114973b46ced8827f8c7a527ee7e
lachlancrabbJCU <140355342+lachlancrabbJCU@users.noreply.github.com>
2024-08-10 21:05:10 +1000

    Refactor for clarity

--- prac_10/wiki.py
+++ prac_10/wiki.py
@@ -10,20 +10,17 @@ def main():
     while page_title != "":
         try:
             page = wikipedia.page(page_title, auto_suggest=False)
-            is_valid_page = True
+            print(page.title)
+            print(page.summary)
+            print(page.url)
         except wikipedia.exceptions.DisambiguationError as disambiguation_error:
             print("We need a more specific title. Try one of the following, or a new search:")
-            print(disambiguation_error)
-        except wikipedia.exceptions.PageError as page_error:
+            print(disambiguation_error.options)
+        except wikipedia.exceptions.PageError:
             print(f'Page id "{page_title}" does not match any pages. Try another id!')
-        print(page.title)
-        print(page.summary)
-        print(page.url)
         print()
         page_title = input("Enter page title: ")
     print("Thank you.")
-
-
 
 
 if __name__ == '__main__':
